0c44d9cc7c575d61fbb6223a1e1d3f6a4786ece7
Monk <ch.monk14@gmail.com>
2016-11-18 22:45:06 +0700

    reply "light"

--- bot.php
+++ bot.php
@@ -30,6 +30,13 @@ if (!is_null($events['events'])) {
                 'text' => $text
             ];
 
+            if(strpos($text, 'light') !== false){
+                $messages1 = [
+                    'type' => 'text',
+                    'text' => 'Light is on'
+                ];
+            }
+
             // Make a POST Request to Messaging API to reply to sender
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
