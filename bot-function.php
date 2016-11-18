420013d3d64bf35104dc6e153ee927885c144fbd
Monk <ch.monk14@gmail.com>
2016-11-19 00:04:20 +0700

    deprecated 'w', multiple return

--- bot-function.php
+++ bot-function.php
@@ -17,18 +17,13 @@ function isLightOn (){
 function isEquation ($receivedStr){
 
     $mixed = $receivedStr;//"100 + 10?";
-    $letters=array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z','?','=');
+    $letters=array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W', 'X', 'Y', 'Z','?','=');
     $only_numbers=str_replace($letters, '', $mixed);
 
     echo "$only_numbers <br>";
 
-    if(ctype_space($only_numbers) || empty($only_numbers))return "False".$only_numbers;
-    else return "True".eval('return '.$only_numbers.';');
+    if(ctype_space($only_numbers) || empty($only_numbers))return false;
+    else return [true, eval('return '.$only_numbers.';')];
 
-//    $only_numbers = eval('return '.$only_numbers.';');
-//
-//    echo "$only_numbers <br>";
-
-    //return $only_numbers;
 
 }
